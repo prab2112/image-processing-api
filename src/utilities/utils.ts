12025457b970ba4473a725b09ab0786c5c76216a
Prabhakar Pandey <prabhakar.pandey@e2open.com>
2022-07-14 08:43:53 +0530

    code changes as per feedback

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,9 +1,9 @@
-const isArrayOfNums = (arr: unknown[]) : boolean => {
-    return arr.every(item => Number.isInteger(item));
+const isArrayOfNums = (arr: unknown[]): boolean => {
+  return arr.every(item => Number.isInteger(item));
 };
 
 const itemsInArray = (arr: unknown[], items: unknown[]): boolean => {
-    return arr.every((item) => items.indexOf(item) !== -1);
-  };
+  return arr.every(item => items.indexOf(item) !== -1);
+};
 
-export {isArrayOfNums , itemsInArray};
+export { isArrayOfNums, itemsInArray };
